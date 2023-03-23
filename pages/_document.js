936065b2006599aac1f926f738c7536a1661ae48
Fonji Terence <terencehilln@gmail.com>
2023-03-24 00:20:17 +0100

    Update Contact

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="fr">
+      <title>
+        Digital Youth Service
+      </title>
       <Head />
+
       <body>
         <Main />
         <NextScript />
--- pages/index.js
+++ pages/index.js
@@ -9,20 +9,31 @@ import Feature from '@/components/Feature/features'
 import Projects from '@/components/Project/projects'
 import Testimonials from '@/components/Testimonials/testimonial'
 import CTA3 from '@/components/CTA/cta3'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Hero />
-      <Feature />
-      <CTA1 />
-      <Projects />
-      <Logo_Clouds />
-      <Testimonials />
-      <CTA3 />
+      <Head>
+        <title>Digital Youth Service</title>
+        <meta
+          name="description"
+          content=" Nous sommes Digital Youth Service Fr, une agence web spécialisée dans la création de solutions numériques pour les entreprises et les particuliers. Nous comprenons que votre entreprise a besoin d'une présence en ligne forte pour réussir et c'est là que nous intervenons."
+        />
+      </Head>
+      <main>
+        <Navbar />
+        <Hero />
+        <Feature />
+        <CTA1 />
+        <Projects />
+        <Logo_Clouds />
+        <Testimonials />
+        <CTA3 />
+      </main>
+
       <Footers />
     </>
   );
